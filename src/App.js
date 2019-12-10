00c34d5d2e1b3572de206318ac5f9bc60242ccbc
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2019-12-10 16:28:33 +0200

    Fixed search input, changed table size

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Hypervisors from './Hypervisors';
 import VirtualMachines from './VirtualMachines';
 import { SideMenu } from './components/SideMenu';
 import { fetchMachines, fetchHyps } from './Api';
+import './App.css';
 
 const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    overflowX: 'auto',
+  rootContainer: {
+    padding: '0 !important'
   },
   table: {
     width: '100%',
@@ -72,29 +71,27 @@ function App() {
   return (
     <div className="App">
       <Box>
-        <Container>
-          <Grid container>
-            <Grid item xs={2}>
-              <SideMenu activeTab={activeTab} onChange={onChangeTab} />
-            </Grid>
-            <Grid item xs={10}>
-              <Paper>
-                <VirtualMachines 
-                  data={virtsData}
-                  classes={classes}
-                  activeTab={activeTab} 
-                  isLoading={isLoading}
-                />
-                <Hypervisors 
-                  data={hypsData}
-                  classes={classes} 
-                  activeTab={activeTab} 
-                  isLoading={isLoading}
-                />
-             </Paper>
-            </Grid>
+        <Grid container>
+          <Grid xs={1}>
+            <SideMenu activeTab={activeTab} onChange={onChangeTab} />
           </Grid>
-        </Container>
+          <Grid xs={11}>
+            <Paper>
+              <VirtualMachines 
+                data={virtsData}
+                classes={classes}
+                activeTab={activeTab} 
+                isLoading={isLoading}
+              />
+              <Hypervisors 
+                data={hypsData}
+                classes={classes} 
+                activeTab={activeTab} 
+                isLoading={isLoading}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
       </Box>
     </div>
   );
