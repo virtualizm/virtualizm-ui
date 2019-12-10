43c3ba57429a05b56f249f0c5d326641b5efb7f4
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2019-12-10 15:52:33 +0200

    Added sorting, filtering, formating data, and custom actions

--- src/TabPanel.jsx
+++ src/TabPanel.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './App.css';
 
 export default function TabPanel({index, activeTab, children}) {
     return index === activeTab ? (
-      <div>{children}</div>
+      <div className='tabPanel'>{children}</div>
     ) : null;
   };
--- src/VirtualMachines.jsx
+++ src/VirtualMachines.jsx
@@ -1,44 +1,82 @@
-import React, { useMemo} from 'react';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
+import React from 'react';
 import TabPanel from './TabPanel';
+import MaterialTable from 'material-table';
+import pretty from 'prettysize';
+import './App.css';
 
-const Hypervisors = ({activeTab, classes, data}) => {
-    const rows = useMemo(() => {
-        if(!data) return [];
+const renderLabel = ({state}) => {
+  const className = state === 'running' ? 'running': 'down';
 
-        return data.map(({id, attributes}) => ({id, ...attributes}));
-    }, [data]);
+  return (
+    <div className={`label ${className}`}>
+      {state}
+    </div>
+  );
+}
 
-    return (
-      <TabPanel activeTab={activeTab} index={0}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>State</TableCell>
-              <TableCell>Memory</TableCell>
-              <TableCell>CPUs</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.state}</TableCell>
-              <TableCell>{row.memory}</TableCell>
-              <TableCell>{row.cpus}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        </Table>
-      </TabPanel>
-    )
+function renderList(data){
+  return (
+    <ul>
+      {Object.keys(data).map((field) => {
+        const label = data[field];
+        const isObject = typeof label === 'object';
+        
+        if(isObject) {
+          return <li>{field} {renderList(label)}</li>;
+        }
+
+        return (
+          <li>{`${field}: ${label}`}</li>
+        )
+      })}    
+    </ul>
+  );
+}
+
+const Modal = ({handleClose, data}) => {
+  return (
+    <div className='modal'>
+      <button onClick={handleClose}>Close</button>
+
+      {renderList(data)}
+    </div>
+  )
+}
+
+const Hypervisors = ({ activeTab, isLoading, data }) => {
+  const [modal, setModal] = React.useState(null);
+
+  const handleOpen = (e, data) => {
+    setModal(data);
+  };
+
+  const handleClose = () => {
+    setModal(null);
+  };
+
+  return (
+    <TabPanel activeTab={activeTab} index={0}>
+      {modal && <Modal data={modal} handleClose={handleClose}/>}
+      <MaterialTable
+        title='Virtual Machines'
+        data={ data }
+        isLoading={isLoading}
+        columns={[
+          { title: 'Id', field: 'id' },
+          { title: 'Name', field: 'name' },
+          { title: 'State', field: 'state', render: renderLabel },
+          { title: 'memory', field: 'memory', render: ({memory}) => pretty(memory)},
+          { title: 'CPUs', field: 'cpus' },
+          { title: 'Hypervisor', field: 'hypName'}
+        ]}
+        actions={[{
+            icon: 'visibility',
+            tooltip: 'Show',
+            onClick: handleOpen
+          }]}
+      />
+    </TabPanel>
+  );
 }
 
 export default Hypervisors;
