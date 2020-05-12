4ab35fb53bafc22c5190d5b28888f4ba0d0292a6
sergey.sinina <sergey.sinina@wildix.com>
2020-05-12 20:53:41 +0300

    added sorting to tables. removed api.js as unused

--- src/features/pages/Hypervisors.jsx
+++ src/features/pages/Hypervisors.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useContext, useCallback } from "react";
 import pretty from "prettysize";
 import { Table, Tag } from "antd";
 import { fetchHypervisors } from "../../utils/api";
+import { sortNumbers, sortStrings } from "../../utils/tableSorters";
 import {
   StoreContext,
   startLoading,
@@ -42,31 +43,63 @@ const Hypervisors = () => {
       isLoading={isLoading}
       columns={[
         { title: "Id", dataIndex: "id", key: "id" },
-        { title: "Name", dataIndex: "name", key: "name" },
+        {
+          title: "Name",
+          dataIndex: "name",
+          key: "name",
+          sorter: (a, b) => sortStrings(a.name, b.name),
+        },
         {
           title: "State",
           dataIndex: "connected",
           key: "connected",
-          render: renderState
+          render: renderState,
+          sorter: (a, b) => sortStrings(a.connected, b.connected),
         },
-        { title: "CPUs", dataIndex: "cpus", key: "cpus" },
-        { title: "Max vCPUs", dataIndex: "max_vcpus", key: "max_vcpus" },
-        { title: "MHz", dataIndex: "mhz", key: "mhz" },
+        {
+          title: "CPUs",
+          dataIndex: "cpus",
+          key: "cpus",
+          sorter: (a, b) => sortNumbers(a.cpus, b.cpus),
+        },
+        {
+          title: "Max vCPUs",
+          dataIndex: "max_vcpus",
+          key: "max_vcpus",
+          sorter: (a, b) => sortNumbers(a.max_vcpus, b.max_vcpus),
+        },
+        {
+          title: "MHz",
+          dataIndex: "mhz",
+          key: "mhz",
+          sorter: (a, b) => sortNumbers(a.mhz, b.mhz),
+        },
         {
           title: "Total Memory",
           dataIndex: "total_memory",
           key: "total_memory",
           render: total_memory => pretty(total_memory),
+          sorter: (a, b) => sortNumbers(a.total_memory, b.total_memory),
         },
         {
           title: "Free Memory",
           dataIndex: "free_memory",
           key: "free_memory",
           render: total_memory => pretty(total_memory),
+          sorter: (a, b) => sortNumbers(a.free_memory, b.free_memory),
         },
-        { title: "QEMU Version", dataIndex: "version", key: "version" },
-        { title: "Libvirt Version", dataIndex: "libversion", key: "libversion" },
-
+        {
+          title: "QEMU Version",
+          dataIndex: "version",
+          key: "version",
+          sorter: (a, b) => sortNumbers(a.version, b.version),
+        },
+        {
+          title: "Libvirt Version",
+          dataIndex: "libversion",
+          key: "libversion",
+          sorter: (a, b) => sortNumbers(a.libversion, b.libversion),
+        }
       ]}
       pagination={{ showSizeChanger: true }}
     />
