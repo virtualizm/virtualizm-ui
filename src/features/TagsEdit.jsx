8ed29614f5a34e853bfa60b90189fcb6744e78a9
Sergey Sinina <serrikk@gmail.com>
2020-06-14 00:20:41 +0300

    fix build

--- src/features/TagsEdit.jsx
+++ src/features/TagsEdit.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Select } from "antd";
 import { EditOutlined, CheckOutlined, CloseOutlined } from "@ant-design/icons";
 import { setVmTags } from "../utils/api";
